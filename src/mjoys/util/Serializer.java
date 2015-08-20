b79b1bc1fd5d976badb322357bc915d5617b9e5b
limeuser <kK644742935@gmail.com>
2015-08-20 18:14:34 +0800

    r

--- src/mjoys/util/Serializer.java
+++
@@ -1,6 +0,0 @@
-package mjoys.util;
-
-public interface Serializer {
-    byte[] encode(Object obj);
-    Object decode(byte[] data);
-}
--- src/mjoys/util/TLVFrame.java
+++ src/mjoys/util/TLVFrame.java
@@ -5,50 +5,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TLVFrame {
-    private int type;
-    private int length;
-    private byte[] value;
+    private short type;
+    private short length;
+    private ByteBuffer value;
     
     public final static int HeadLength = 4;
     
-    public int getType() {
+    public short getType() {
         return type;
     }
-    public void setType(int type) {
+    public void setType(short type) {
         this.type = type;
     }
-    public int getLength() {
+    public short getLength() {
         return length;
     }
-    public void setLength(int length) {
+    public void setLength(short length) {
         this.length = length;
     }
-    public byte[] getValue() {
+    public ByteBuffer getValue() {
         return value;
     }
-    public void setValue(byte[] value) {
+    public void setValue(ByteBuffer value) {
         this.value = value;
     }
     
-    public static final List<TLVFrame> parseTLVFrames(byte[] buffer, int offset, int length) {
+    public static final List<TLVFrame> parseTLVs(ByteBuffer bf) {
         List<TLVFrame> frames = new ArrayList<TLVFrame>();
-        int remainLength = length;
-        ByteBuffer bf = ByteBuffer.wrap(buffer, offset, length);
         
-        while (remainLength > HeadLength) {
-            int type = bf.getShort();
-            int valueLength = bf.getShort();
-            remainLength -= HeadLength;
+        while (bf.remaining() > HeadLength) {
+            short type = bf.getShort();
+            short valueLength = bf.getShort();
             
-            if (remainLength >= valueLength) {
+            if (bf.remaining() >= valueLength) {
                 TLVFrame frame = new TLVFrame();
                 frame.setType(type);
                 frame.setLength(valueLength);
-                frame.setValue(new byte[valueLength]);
-                System.arraycopy(buffer, HeadLength, frame.getValue(), 0, valueLength);
-                
+                ByteBuffer value = bf.slice();
+                value.limit(bf.position() + valueLength);
+                bf.position(bf.position() + valueLength);
+                frame.setValue(value);
                 frames.add(frame);
-                remainLength -= valueLength;
             } else {
                 break;
             }
@@ -57,27 +54,11 @@ public class TLVFrame {
         return frames;
     }
     
-    public static final List<TLVFrame> parseTLVFrames(byte[] buffer, int length) {
-        return parseTLVFrames(buffer, 0, length);
-    }
-    
-    public static final List<TLVFrame> parseTLVFrames(byte[] buffer) {
-        return parseTLVFrames(buffer, 0, buffer.length);
-    }
-    
-    public static final TLVFrame parseTLVFrame(byte[] buffer, int offset, int length) {
-        List<TLVFrame> frames = parseTLVFrames(buffer, offset, length);
+    public static final TLVFrame parseTLV(ByteBuffer buffer) {
+        List<TLVFrame> frames = parseTLVs(buffer);
         if (frames.size() != 1) {
             return null;
         }
         return frames.get(0);
-    }
-    
-    public static final TLVFrame parseTLVFrame(byte[] buffer, int length) {
-        return parseTLVFrame(buffer, 0, length);
-    }
-    
-    public static final TLVFrame parseTLVFrame(byte[] buffer) {
-        return parseTLVFrame(buffer, 0, buffer.length);
     }
 }
